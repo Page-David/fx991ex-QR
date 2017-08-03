243d50c450653ace8f0ffe16c4ed9746f753780e
Page David <david991010@gmail.com>
2017-08-03 16:22:46 +0800

    dynamic setup

---
+++ web/js/home.js
@@ -0,0 +1,28 @@
+function parser() {
+	var res_url = document.getElementById("url_res");
+	var in_mul = document.getElementById("in_mul");
+	_compose = res_url.value.split("+");
+	console.log(_compose[5])
+	var U_compose = _compose[1];
+	var M_compose = _compose[2];
+	var set_up = _compose[3];
+	var cal_res = _compose[4];
+	var equa = _compose[5];
+	if (!equa) {
+		in_mul.innerHTML = "";
+		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	}
+	var parser_res = "";
+	for (i = 2; i < equa.length; i += 2) {
+		parser_res += mapping(equa.slice(i, i+2));
+	}
+	in_mul.innerHTML = "$$" + parser_res + "$$";
+	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+}
+
+function mapping(inner_key) {
+	var map_dict = {
+		"22" : "\\pi"
+		};
+	return map_dict[inner_key];
+}
